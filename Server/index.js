14c9a08270a60720d09cf42f3676f90690be50d5
Pablo Risi <pablomrisi@hotmail.com>
2026-07-21 20:07:15 -0300

    cors deploey

--- Server/index.js
+++ Server/index.js
@@ -42,6 +42,19 @@ const corsOptions = {
     optionsSuccessStatus: 200
 };
 
+// Middleware CORS manual (más permisivo)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
